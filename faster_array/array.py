1eaa799d57efac939c0d73f70da576a0cd81ee57
Kaushik Kulkarni <kaushikcfd@gmail.com>
2018-12-19 11:12:27 -0600

    Extended the brodcasting logic and added the reshape! :)

--- faster_array/array.py
+++ faster_array/array.py
@@ -4,7 +4,7 @@ from numbers import Number
 import islpy as isl
 from pymbolic.primitives import Variable, Call, Subscript
 from pymbolic import parse
-from loopy.isl_helpers import make_slab
+from loopy.isl_helpers import make_slab, simplify_via_aff
 
 
 class ArraySymbol(lp.ArrayArg):
@@ -26,6 +26,11 @@ class ArraySymbol(lp.ArrayArg):
 
         kwargs["address_space"] = address_space
         kwargs["stack"] = stack
+        # setting the default order as C
+        # note that this state is not stored, instead reflected in
+        # :attr:dim_tags
+        kwargs["order"] = kwargs.pop("order", 'C')
+        self.order = kwargs["order"]
 
         super(ArraySymbol, self).__init__(*args, **kwargs)
 
@@ -64,32 +69,83 @@ class ArraySymbol(lp.ArrayArg):
             return self.copy(name=subst_name,
                 dtype=dtype)
         elif isinstance(other, ArraySymbol):
-            if other.shape == (1, ):
-                # extend this to the broadcasting logic
+            if self.shape == other.shape:
                 inames = tuple(
                        self.stack.name_generator(based_on='i') for _ in
                        self.shape)
-                rhs, dtype = _apply_op(Call(function=Variable(self.name),
-                        parameters=tuple(Variable(iname) for iname in inames)),
-                        Variable(other.name)())
+                rhs, dtype = _apply_op(
+                        Variable(self.name)(*tuple(Variable(iname) for iname in
+                            inames)),
+                        Variable(other.name)(*tuple(Variable(iname) for iname
+                            in inames)))
                 subst_name = self.stack.name_generator(based_on='subst')
-                self.stack.register_substitution(lp.SubstitutionRule(
-                        subst_name, inames, rhs))
+                self.stack.register_substitution(lp.SubstitutionRule(subst_name,
+                        inames, rhs))
                 return self.copy(name=subst_name, dtype=dtype)
+            else:
+                left = self
+                right = other
+                new_left_shape = self.shape[:]
+                new_right_shape = other.shape[:]
+                if len(left.shape) > len(right.shape):
+                    new_right_shape = (
+                            (1, )*(len(left.shape)-len(right.shape))
+                            + new_right_shape)
+                if len(left.shape) < len(right.shape):
+                    new_left_shape = (
+                            (1, )*(len(right.shape)-len(left.shape))
+                            + new_left_shape)
 
-            if not self.shape == other.shape:
-                raise NotImplementedError('At this moment broadcasting is not '
-                        'supported. But support for it will be added soon!')
-            inames = tuple(
-                   self.stack.name_generator(based_on='i') for _ in
-                   self.shape)
-            rhs, dtype = _apply_op(Variable(self.name)(*tuple(Variable(iname) for
-                iname in inames)), Variable(other.name)(*tuple(Variable(iname) for
-                iname in inames)))
-            subst_name = self.stack.name_generator(based_on='subst')
-            self.stack.register_substitution(lp.SubstitutionRule(subst_name,
-                    inames, rhs))
-            return self.copy(name=subst_name, dtype=dtype)
+                # now both have the same shapes
+                # let compute the shape of these guys
+                new_shape = [1]*len(new_left_shape)
+
+                for i, (left_len, right_len) in enumerate(zip(new_left_shape,
+                        new_right_shape)):
+                    if left_len == right_len:
+                        new_shape[i] = left_len
+                    else:
+                        if left_len != 1 and right_len != 1:
+                            raise ValueError("Cannot be broadcasted.")
+                        else:
+                            new_shape[i] = left_len*right_len
+
+                new_shape = tuple(new_shape)
+
+                if new_left_shape != left.shape:
+                    subst_name = self.stack.name_generator(based_on="subst")
+                    inames = tuple(self.stack.name_generator(based_on="i") for
+                            _ in new_left_shape)
+                    indices = tuple(Variable(iname) if axis_len != 1 else 0 for
+                            iname, axis_len in zip(inames[-len(left.shape):],
+                                left.shape))
+                    rule = lp.SubstitutionRule(left.name,
+                            inames,
+                            Variable(left.name)(*indices))
+                    self.stack.register_substitution(rule)
+                    new_left = left.copy(stack=self.stack, name=subst_name,
+                            shape=new_shape, dim_tags=None, order=left.order)
+                else:
+                    new_left = left
+
+                if new_right_shape != right.shape:
+                    subst_name = self.stack.name_generator(based_on="subst")
+                    inames = tuple(self.stack.name_generator(based_on="i") for
+                            _ in new_right_shape)
+                    indices = tuple(Variable(iname) if axis_len != 1 else 0 for
+                            iname, axis_len in zip(inames[-len(right.shape):],
+                                right.shape))
+                    rule = lp.SubstitutionRule(subst_name,
+                            inames,
+                            Variable(right.name)(*indices))
+                    self.stack.register_substitution(rule)
+                    new_right = right.copy(stack=self.stack, name=subst_name,
+                            shape=new_shape, dim_tags=None, order=right.order)
+                else:
+                    new_right = right
+
+                return new_left._arithmetic_op(new_right, op)
+
         else:
             raise NotImplementedError('__mul__ for', type(other))
 
@@ -197,6 +253,49 @@ class ArraySymbol(lp.ArrayArg):
             self.stack.substs_to_arrays[subst_name] = arg_name
             self.name = subst_name
 
+    def reshape(self, new_shape, order='C'):
+        # need an error here complain if there is a shape mismatch
+        # how to do this:
+        # look at how loopy sets its dim tags, from shape and order.
+        subst_name = self.stack.name_generator(based_on='subst')
+        inames = tuple(self.stack.name_generator(based_on="i") for _ in new_shape)
+        new_arg = self.copy(
+                stack=self.stack,
+                name=subst_name,
+                shape=new_shape,
+                dim_tags=None,
+                order=order)
+
+        linearized_idx = sum(Variable(iname)*dim_tag.stride for iname, dim_tag in
+                zip(inames, new_arg.dim_tags))
+        strides = tuple(dim_tag.stride for dim_tag in self.dim_tags)
+        if self.dim_tags[0].stride == 1:
+            pass
+        elif self.dim_tags[-1].stride == 1:
+            strides = strides[::-1]
+        else:
+            raise ValueError()
+
+        indices = []
+        for stride in strides[::-1]:
+            current_idx = linearized_idx // stride
+            indices.append(simplify_via_aff(current_idx))
+            linearized_idx -= current_idx*stride
+
+        # should we assert that the linearized index is 0?
+
+        if self.dim_tags[-1].stride == 1:
+            indices = indices[::-1]
+
+        rule = lp.SubstitutionRule(
+                subst_name,
+                inames,
+                expression=Variable(self.name)(*tuple(indices)))
+
+        self.stack.register_substitution(rule)
+
+        return ArraySymbol(stack=self.stack, name=subst_name, shape=new_shape)
+
     def __getitem__(self, index):
         if isinstance(index, Number):
             index = (index, )
@@ -211,6 +310,9 @@ class ArraySymbol(lp.ArrayArg):
             elif isinstance(idx, slice):
                 # right now only support complete slices
                 # future plan is to make it diverse by adding it more support
+                # its easy, anyone intereseted can do it, with a small change
+                # to make_slab, or wait for kernel_callables_v3 to go through
+                # into master
                 assert idx.start is None
                 assert idx.stop is None
                 assert idx.step is None
